db25aea712702256acf83c109bb21a2ecc522917
mariusc46 <69582884+mariusc46@users.noreply.github.com>
2021-06-30 15:06:21 +0300

    last round of review fixes (#2)
    * last round of review fixes
    
    * Final src fix
    
    Co-authored-by: mariusc46 <marius.cuuntan@mail.com>

--- UDP Server - C++/src/main.cpp
+++ UDP Server - C++/src/main.cpp
@@ -4,14 +4,13 @@
 #include "MyServer.hpp"
 
 const std::string IP = "127.0.0.1";
-const int PORT = 4000;
+constexpr int PORT = 4000;
 
 int main(int argc, char *argv[])
 {
-    udp_listener::MyServerClass myServerObj(IP, PORT);
-
     try
     {
+        udp_listener::MyServerClass myServerObj(IP, PORT);
         myServerObj.receiveFrames();
     }
     catch (int e)
