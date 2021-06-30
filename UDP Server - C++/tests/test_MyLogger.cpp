3b10a00959d5ebab911f48ab03c383902ee99f3a
mariusc46 <marius.cuuntan@mail.com>
2021-06-30 16:26:24 +0300

    Dummy first test

---
+++ UDP Server - C++/tests/test_MyLogger.cpp
@@ -0,0 +1,24 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "../src/MyLogger.hpp"
+
+class MyLoggerTest: public ::testing::Test
+{
+    protected:
+    udp_listener::MyLogger logger;
+};
+
+TEST_F(MyLoggerTest, log_error)
+{
+    testing::internal::CaptureStdout();
+    logger.LogError();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("CRC"), std::string::npos);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
