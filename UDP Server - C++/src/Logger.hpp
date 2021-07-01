0e922ae6e62df5d9837d93f33102a5a0dce4d1a5
mariusc46 <69582884+mariusc46@users.noreply.github.com>
2021-07-01 12:46:18 +0300

    Delete UDP Server - C++ directory

--- UDP Server - C++/src/Logger.hpp
+++
@@ -1,9 +0,0 @@
-namespace udp_listener
-{
-    class Logger
-    {
-    public:
-        virtual void LogMessage(int64_t timestamp, uint32_t mileage, float speed) = 0;
-        virtual void LogError() = 0;
-    };
-}
